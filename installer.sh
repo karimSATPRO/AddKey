710f4903d4004ae4bdebe39145a7019b2f5354d9
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2022-12-09 03:33:02 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 
-echo "############### stalker.conf #################"
-echo "############ BY WOLRD OF ENIGMA2 #################"
+echo "############### enigma2-plugin-extensions-keyadder_v1.1_all.ipk #################"
+echo "############ KEYADDER[Bissfeed] #################"
 
 wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.1_all.ipk "https://github.com/karimSATPRO/AddKey/blob/main/enigma2-plugin-extensions-keyadder_v1.1_all.ipk?raw=true"
 
