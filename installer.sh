6b8038a499d1a7a84e7fea222ad7f4a1e87a0563
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2022-12-09 03:15:42 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,1 +1,8 @@
-# AddKey
+
+
+
+wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.1_all.ipk "https://github.com/karimSATPRO/AddKey/blob/main/enigma2-plugin-extensions-keyadder_v1.1_all.ipk?raw=true"
+
+opkg install --force-overwrite /tmp/*.ipk
+
+exit 0
