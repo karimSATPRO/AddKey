b89546267315b14315929a435a8433f1e6739879
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2022-12-09 03:28:06 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,8 +1,17 @@
 
-
+echo "############### stalker.conf #################"
+echo "############ BY WOLRD OF ENIGMA2 #################"
 
 wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.1_all.ipk "https://github.com/karimSATPRO/AddKey/blob/main/enigma2-plugin-extensions-keyadder_v1.1_all.ipk?raw=true"
 
 opkg install --force-overwrite /tmp/*.ipk
 
+echo ""
+cd ..
+sync
+echo "############ INSTALLATION termine ########"
+echo "############ Restart Enigma2 GUI... #################" 
+init 4
+sleep 2
+init 3
 exit 0
